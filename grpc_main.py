063db0b786b4779b385233fee31446b5eaa44f08
libra <libra@libras-MacBook-Pro.local>
2026-05-13 10:28:53 +0700

    chore: use dotenv instead of manual config fields

--- grpc_main.py
+++ grpc_main.py
@@ -3,11 +3,17 @@ import grpc
 import v1.ocr_image_pb2 as data_pb2
 import v1.ocr_image_pb2_grpc as data_pb2_grpc
 from rpc_provider.ocr_service import ImageOCRService
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+WORKER = os.getenv("GRPC_WORKER",8)
+PORT = os.getenv("GRPC_PORT", 50051)
 
 def serveOCR():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=WORKER))
     data_pb2_grpc.add_ImageOCRServiceServicer_to_server(ImageOCRService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port(f'[::]:{PORT}')
     server.start()
 
     server.wait_for_termination()
